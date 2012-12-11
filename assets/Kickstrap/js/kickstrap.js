c405b027af19611e73e5cd180d256b4969f0abc4
Adam Kochanowicz <ajkochanowicz@gmail.com>
2012-12-11 10:03:25 -0500

    adding back in IE detection

--- assets/Kickstrap/js/kickstrap.js
+++ assets/Kickstrap/js/kickstrap.js
@@ -62,6 +62,19 @@ ks.testParams = { readyCount: 0 }
 // FUNCTIONS
 // =========
 
+// Those with IE shall be marked.
+var ver = getInternetExplorerVersion();
+function getInternetExplorerVersion() {
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer') {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
 // The actual mega function ks.ready() will call to run all ready fxs.
 
 function kickstrapReady() {
