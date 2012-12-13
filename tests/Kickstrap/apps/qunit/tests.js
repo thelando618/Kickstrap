da75d02bf17d4bdc7353aa48492e319186bd6bc6
Adam Kochanowicz <ajkochanowicz@gmail.com>
2012-12-13 14:57:07 -0500

    ugh, adding all the files back.

---
+++ tests/Kickstrap/apps/qunit/tests.js
@@ -0,0 +1,6 @@
+// Write your tests here or in kickstrap.ready() to prevent them from loading before QUnit does.
+
+test("a basic unit test", function() {
+  var value="hello"
+  equal(value,"hello","We expect value to be hello")
+});
